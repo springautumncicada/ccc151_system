7cd4951ece4fff2c2f94833de873655e5278557b
eculipseNamae <69389203+eculipseNamae@users.noreply.github.com>
2024-05-18 04:49:27 +0800

    heri folder

---
+++ heri/public/app.js
@@ -0,0 +1,291 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Fetch sensor and then display sensor status
+  fetch('/sensor')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(sensordata => {
+    updateSensorTable(sensordata);
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+  });
+  
+  fetch('/conclusion')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(conclusion => {
+    updateConclusionTable(conclusion);
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+  });
+
+  fetch('/data')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(datareport => {
+    updateDatareportTable(datareport);
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+  });
+
+  const addForm = document.querySelector('form#add-datareport-form');
+  const deleteForm = document.querySelector('form#delete-datareport-form');
+  const updateForm = document.querySelector('form#update-datareport-form');
+  addForm.addEventListener('submit', event => {
+    alert("Add form");
+    event.preventDefault();
+    adddatareport();
+    addForm.reset();
+  });
+  deleteForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const id = deleteForm.elements['id'].value;
+    deleteDataReport(id);
+    deleteForm.reset();
+  });
+  updateForm.addEventListener('submit', event => {
+    alert("Update form");
+    event.preventDefault();
+    const id = updateForm.elements['updatedatareport-id'].value;
+    const measurement_id = updateForm.elements['updatemeasurement-id'].value;
+    const conclusion_id = updateForm.elements['updateconclusion-id'].value;
+    
+    updateData(id, measurement_id, conclusion_id);
+    updateForm.reset();
+  });
+
+  document.getElementById('add-cases-button').addEventListener('click', function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+  
+    // Get the value from the text field
+    const inputValue = document.getElementById('add-cases-desc').value;
+    
+    // Send a POST request to your server
+    fetch('/caseadd', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ input : inputValue }) // Assuming your server expects JSON data
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const tableBody = document.querySelector('table#cases tbody');
+      const newRowHTML = `
+        <tr>
+          <td>${inputValue}</td>
+        </tr>
+      `;
+      tableBody.insertAdjacentHTML('beforeend', newRowHTML);
+      return response.json();
+    })
+    .then(data => {
+      // Handle the response from the server
+      console.log('Server response:', data);
+    })
+    .catch(error => {
+      console.error('Error sending POST request:', error);
+    });
+  });
+  
+  
+  
+});
+
+function updateSensorText() {
+}
+// Update the table with the fetched users
+function updateSensorTable(sensordata) {
+  console.log('Received data:', sensordata);
+  const tableBody = document.querySelector('table#sensor-stats tbody');
+  tableBody.innerHTML = '';
+  
+  sensordata.forEach(sensor => {
+    const row = document.createElement('tr');
+    const statusCell = document.createElement('td');
+    statusCell.textContent = sensor.status;
+    row.appendChild(statusCell);
+    tableBody.appendChild(row);
+  });
+}
+
+function updateDatareportTable(datareport) {
+  console.log('Received data:', datareport);
+  const tableBody = document.querySelector('table#data-report tbody');
+  tableBody.innerHTML = '';
+  
+  datareport.forEach(data => {
+    const row = document.createElement('tr');
+    const idCell = document.createElement('td');
+    idCell.textContent = data.id;
+    const dataCell = document.createElement('td');
+    dataCell.textContent = data.data;
+    const descriptionCell = document.createElement('td');
+    descriptionCell.textContent = data.conclusion;
+    const timeCell = document.createElement('td');
+    timeCell.textContent = data.time;
+    row.appendChild(idCell);
+    row.appendChild(dataCell);
+    row.appendChild(descriptionCell);
+    row.appendChild(timeCell);
+    tableBody.appendChild(row);
+  });
+}
+
+function updateConclusionTable(conclusion) {
+  console.log('Received data:', conclusion);
+  const tableBody = document.querySelector('table#cases tbody');
+  tableBody.innerHTML = '';
+  
+  conclusion.forEach(cases => {
+    const row = document.createElement('tr');
+    const descriptionCell = document.createElement('td');
+    descriptionCell.textContent = cases.description;
+    row.appendChild(descriptionCell);
+    tableBody.appendChild(row);
+  });
+}
+
+function adddatareport() {
+  const measurement_id = document.getElementById('measurement-id').value;
+  const conclusion_id = document.getElementById('conclusion-id').value;
+  const userData = { measurement_id, conclusion_id };
+
+  if (!measurement_id || !conclusion_id) {
+    alert('Please fill in all required fields.');
+    return;
+  }
+
+  fetch('/adddatareport', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Failed to add data report');
+    }
+  })
+  .then(datareport => {
+    console.log("Added data report: ", datareport);
+    alert(`Data Report ${datareport.id} added successfully`);
+
+    // Fetch updated data reports from the correct endpoint
+    return fetch('/data');
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Failed to fetch data reports');
+    }
+  })
+  .then(datareports => {
+    updateDatareportTable(datareports);
+  })
+  .catch(error => {
+    console.error('Error: ', error);
+    alert('An error occurred');
+  });
+}
+
+function deleteDataReport(id) {
+  fetch(`/deletedata/${id}`, {
+    method: 'DELETE'
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Failed to delete user');
+    }
+  })
+  .then(result => {
+    if (result.id) {
+      fetch('/data')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Failed to fetch users');
+        }
+      })
+      .then(datareports => {
+        updateDatareportTable(datareports);
+      })
+      .catch(error => {
+        console.error('Error fetching users: ', error);
+        alert('Failed to fetch users');
+      });
+    } else {
+      throw new Error('Failed to delete user');
+    }
+  })
+  .catch(error => {
+    console.error(error);
+    alert('Failed to delete user');
+  });
+}
+
+function updateData(id, measurement_id, conclusion_id) {
+  const userData = { measurement_id, conclusion_id};
+  
+  fetch(`/updatedata/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Failed to update user');
+    }
+  })
+  .then(user => {
+    console.log("Updated user: ", user);
+    alert(`User ${user.measurement_id} updated successfully`);
+    fetch('/data')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to fetch users');
+      }
+    })
+    .then(datareports => {
+      updateDatareportTable(datareports);
+    })
+    .catch(error => {
+      console.error('Error fetching users: ', error);
+      alert('Failed to fetch users');
+    });
+  })
+  .catch(error => {
+    console.error(error);
+    alert('Failed to update user');
+  });
+}
---
+++ heri/server.js
@@ -0,0 +1,193 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql2');
+const path = require('path');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// Set up MySQL connection
+const connection = mysql.createConnection({
+//You can use localhost or 127.0.0.1
+//host: 'localhost',
+  host: '127.0.0.1',
+  user: 'root',
+  password: '1234',
+//NOTE: change database name accordingly
+  database: 'uav'
+});
+
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+// Serve the main HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+
+
+// Set up API routes
+app.get('/sensor', (req, res) => {
+  connection.query('SELECT * FROM sensor', (err, results) => {
+    if (err) {
+      console.error('Error executing the database query: ', err);
+      res.status(500).send('Error fetching users');
+      return;
+    }
+
+    const sensor = results.map(row => ({
+      id: row.Sensor_ID,
+      type: row.Sensor_Type,
+      datatype: row.Data_type,
+      status: row.Status
+    }));
+
+    res.json(sensor);
+  });
+});
+
+app.get('/conclusion', (req, res) => {
+  connection.query('SELECT * FROM conclusion', (err, results) => {
+    if (err) {
+      console.error('Error executing the database query: ', err);
+      res.status(500).send('Error fetching users');
+      return;
+    }
+
+    const conclusion = results.map(row => ({
+      id: row.Conclusion_ID,
+      description: row.Case_description,
+    }));
+
+    res.json(conclusion);
+  });
+});
+// Route for fetching data from both 'datareport' and 'measurement' tables
+app.get('/data', (req, res) => {
+  const sql = `
+    SELECT datareport.datareport_id, measurement.raw_data, conclusion.case_description, measurement.time_captured
+    FROM datareport
+    JOIN measurement ON datareport.measurement_id = measurement.measurement_id
+    JOIN conclusion ON datareport.conclusion_id = conclusion.conclusion_id;
+  `;
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error fetching data from both tables:', err);
+      res.status(500).json({ error: 'Error fetching data from both tables' });
+      return;
+    }
+
+    const data = results.map(row => ({
+      id: row.datareport_id,
+      data: row.raw_data,
+      conclusion: row.case_description,
+      time: row.time_captured
+    }));
+
+    res.json(data);
+  });
+});
+
+// Assuming you have already imported required libraries and established a connection to the database
+
+app.post('/caseadd', (req, res) => {
+  // Access the data sent from the client
+  const { input } = req.body;
+  
+  // Define the SQL query
+  const sql = `
+    INSERT INTO conclusion (case_description)
+    VALUES (?);
+  `;
+  
+  // Execute the SQL query
+  connection.query(sql, [input], (err, results) => {
+    if (err) {
+      console.error('Error inserting data:', err);
+      // Send an error response to the client
+      return res.status(500).json({ error: 'Error inserting data into the database' });
+    }
+    
+    console.log('Data inserted successfully');
+    // Send a success response to the client
+    res.json({ message: 'Data inserted successfully' });
+  });
+
+  // Log the received data
+  console.log('Received input:', input);
+});
+
+app.post('/adddatareport', (req, res) => {
+  // Handle adding a new data report to the database
+  const { measurement_id, conclusion_id } = req.body;
+  const dataReport = { measurement_id, conclusion_id };
+
+  if (!measurement_id || !conclusion_id) {
+    return res.status(400).json({ error: "Measurement ID and Conclusion ID are required." });
+  }
+
+  connection.query('INSERT INTO datareport SET ?', dataReport, (err, result) => {
+    if (err) {
+      console.error('Error executing the database query: ', err);
+      res.status(500).send('Error adding data report');
+      return;
+    }
+
+    const insertedDataReport = {
+      id: result.insertId,
+      measurement_id: dataReport.measurement_id,
+      conclusion_id: dataReport.conclusion_id
+    };
+
+    res.send(insertedDataReport);
+  });
+});
+
+app.delete('/deletedata/:id', (req, res) => {
+  const id = req.params.id;
+
+  connection.query('DELETE FROM datareport WHERE datareport_id = ?', [id], (err, result) => {
+    if (err) {
+      console.error('Error executing the database query: ', err);
+      return res.status(500).send('Error deleting user');
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "User not found." });
+    }
+
+    res.json({ id });
+  });
+});
+
+app.put('/updatedata/:id', (req, res) => {
+  const { measurement_id, conclusion_id } = req.body;
+  const id = req.params.id;
+
+  if (!measurement_id || !conclusion_id) {
+    return res.status(400).json({ error: "Name, email, and age are required." });
+  }
+
+  connection.query('UPDATE datareport SET measurement_id = ?, conclusion_id = ? WHERE datareport_id = ?', [measurement_id, conclusion_id, id], (err, result) => {
+    if (err) {
+      console.error('Error executing the database query: ', err);
+      return res.status(500).send('Error updating user');
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "User not found." });
+    }
+
+    res.json({ id, measurement_id, conclusion_id});
+  });
+});
+
+
+
+const PORT = process.env.PORT || 3300;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
